9d1575571780af69f00125d3bac51e487b09f1bd
Anderson Bruno de Lima Santos <anderson.bruno@hbsis.com.br>
2022-02-03 16:21:58 -0300

    Added csv-parser to the project

--- src/modules/cars/useCases/importCategory/Index.ts
+++ src/modules/cars/useCases/importCategory/Index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCateroryUseCase } from './ImportCategoryUseCase';
 
-const importCategoryUseCase = new ImportCateroryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCateroryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export { importCategoryController };
